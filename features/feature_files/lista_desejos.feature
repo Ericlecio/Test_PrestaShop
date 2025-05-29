19c06df375fc61a3ed47b8c05b73597e7462f551
Ericlecio Thiago <101409162+Ericlecio@users.noreply.github.com>
2025-05-28 23:16:13 -0300

    TASK-SCRUM-T68: Implementando [Gherkin] PF - Finalização de Compra

--- features/feature_files/lista_desejos.feature
+++ features/feature_files/lista_desejos.feature
@@ -1,11 +1,18 @@
 Funcionalidade: Lista de Desejos
 
-  Background:
-    Dado que o usuário já esteja logado no sistema
+Contexto:
+  Dado que o site PrestaShop Demo seja acessado
+  E que o usuário clique no botão Sign In
+  E que o usuário clique para criar uma nova conta
+  E que o usuário preencha todos os campos obrigatórios do cadastro
+  Quando o usuário clicar no botão Save
+  Então o sistema deverá registrar o usuário com sucesso
 
-  Cenário: Adicionar produto à lista de desejos e visualizar na My Wishlists
-    Quando eu escolho o primeiro produto e clico no ícone de coração para adicionar à lista de desejos
-    E no modal "Add to wishlist" confirmo a adição do produto
-    E eu clico no meu nome na aba de notificações para acessar o perfil
-    E entro na página "My wishlists"
-    Então o produto adicionado deve estar visível na lista de desejos
+Cenário: Adicionar produto à lista de desejos e visualizar na My Wishlists
+  Quando eu escolho o primeiro produto e clico no ícone de coração para adicionar à lista de desejos
+  E no modal "Add to wishlist" confirmo a adição do produto
+  E eu clico no meu nome na aba de notificações para acessar o perfil
+  E entro na página "My wishlists"
+  E eu clico no item "My wishlist" na lista de desejos
+  Então o produto adicionado é mostrado
+
--- features/pages/wishlist_page.py
+++ features/pages/wishlist_page.py
@@ -1,6 +1,5 @@
 import time
 from features.helpers.driver import get_driver
-from features.pages.base_page import *
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -14,63 +13,66 @@ CONFIRMAR_ADD = "ul.wishlist-list li.wishlist-list-item p"
 NOME_PERFIL = "a.account"
 LINK_MY_WISHLISTS = "a#wishlist-link"
 WISHLIST_PRODUTO = "a.wishlist-list-item-link p.wishlist-list-item-title"
+WISHLIST_ITEM = "li.wishlist-list-item-default a.wishlist-list-item-link p.wishlist-list-item-title"
 
-def acessar_pagina_principal():
-    driver = get_driver()
-    driver.get(URL_PRINCIPAL)
-    try:
-        iframe = WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located((By.TAG_NAME, "iframe"))
-        )
-        driver.switch_to.frame(iframe)
-    except:
-        pass
-    time.sleep(10)
 
 def clicar_primeiro_produto_favorito():
     driver = get_driver()
     driver.get(URL_PRINCIPAL)
-    iframe = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.TAG_NAME, "iframe"))
-    )
+    iframe = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.TAG_NAME, "iframe")))
     driver.switch_to.frame(iframe)
-    time.sleep(10)
-    primeiro_produto = WebDriverWait(driver, 15).until(
-        EC.element_to_be_clickable((By.CSS_SELECTOR, PRIMEIRO_PRODUTO))
-    )
+    time.sleep(3)  
+    
+    primeiro_produto = WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.CSS_SELECTOR, PRIMEIRO_PRODUTO)))
+    
+    driver.execute_script("arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'});", primeiro_produto)
+    time.sleep(2) 
+    
     botao_coracao = primeiro_produto.find_element(By.CSS_SELECTOR, ICONE_CORACAO)
     botao_coracao.click()
-
+    time.sleep(2)
+    driver.execute_script("arguments[0].scrollIntoView({behavior: 'smooth', block: 'start'});", primeiro_produto)
 
 def confirmar_adicao_wishlist():
     driver = get_driver()
-    modal = WebDriverWait(driver, 10).until(
-        EC.visibility_of_element_located((By.CSS_SELECTOR, MODAL_ADD_WISHLIST))
-    )
+    modal = WebDriverWait(driver, 15).until(EC.visibility_of_element_located((By.CSS_SELECTOR, MODAL_ADD_WISHLIST)))
+    time.sleep(3)  
     confirmar = modal.find_element(By.CSS_SELECTOR, CONFIRMAR_ADD)
     confirmar.click()
+    time.sleep(5)
 
 def clicar_nome_perfil():
     driver = get_driver()
-    nome = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.CSS_SELECTOR, NOME_PERFIL))
-    )
+    nome = WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.CSS_SELECTOR, NOME_PERFIL)))
+    time.sleep(3)
     nome.click()
+    time.sleep(3) 
 
 def acessar_my_wishlists():
     driver = get_driver()
-    link = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.CSS_SELECTOR, LINK_MY_WISHLISTS))
-    )
+    link = WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.CSS_SELECTOR, LINK_MY_WISHLISTS)))
+    time.sleep(3)
     link.click()
+    time.sleep(3)
 
 def produto_esta_visivel():
     driver = get_driver()
     try:
-        # Espera o item aparecer na lista de desejos
-        produto = WebDriverWait(driver, 10).until(
-            EC.visibility_of_element_located((By.CSS_SELECTOR, WISHLIST_PRODUTO))
+        WebDriverWait(driver, 15).until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, "div.wishlist-product-image img[alt='Hummingbird printed t-shirt']"))
         )
+        time.sleep(3)  
         return True
-    except:
+    except Exception as e:
+        print(f"Erro: {e}")
         return False
+
+def clicar_my_wishlist_item():
+    driver = get_driver()
+    elemento = WebDriverWait(driver, 15).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, WISHLIST_ITEM))
+    )
+    elemento.click()
+    time.sleep(3)
+    
+    
